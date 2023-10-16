9e78105b15753d8428bb22554aa30d3f1256038d
GBApple <102351624+GBApple@users.noreply.github.com>
2023-10-16 23:15:54 +0900

    Add files via upload
    add clearEntity method

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -209,6 +209,22 @@ public final class DrawManager {
 	}
 
 	/**
+ 	 * Clears a previously drawn entity by drawing the background over its position.
+ 	 *
+ 	 * @param entity Entity to be cleared.
+ 	 * @param positionX X-coordinate of the entity's position.
+ 	 * @param positionY Y-coordinate of the entity's position.
+ 	 * @param screen Screen where the entity is cleared.
+ 	 */
+	public void clearEntity(final Entity entity, final int positionX, final int positionY) {
+    	int entityWidth = entity.getSpriteType().ordinal().getWidth(); // Assuming you have a getWidth() method in your SpriteType enum
+    	int entityHeight = entity.getSpriteType().ordinal().getHeight(); // Assuming you have a getHeight() method in your SpriteType enum
+
+    	backBufferGraphics.setColor(Color.BLACK); // You can use any appropriate color for the background
+    	backBufferGraphics.fillRect(positionX, positionY, entityWidth, entityHeight);
+	}
+
+	/**
 	 * For debugging purpouses, draws the canvas borders.
 	 *
 	 * @param screen
