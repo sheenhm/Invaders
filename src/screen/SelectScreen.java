1b8b42af411e5996af88f325a22f195dcd9be762
Nutria <81311826+Nutriatree@users.noreply.github.com>
2023-12-04 10:56:20 +0900

    Revert "Refactor SelectScreen"

--- src/screen/SelectScreen.java
+++ src/screen/SelectScreen.java
@@ -6,28 +6,28 @@ import engine.SoundManager;
 
 import java.awt.event.KeyEvent;
 
-public class SelectScreen extends Screen {
+public class SelectScreen extends Screen{
 
-    /** 사용자 선택 간의 밀리초. */
+    /** Milliseconds between changes in user selection. */
     private static final int SELECTION_TIME = 200;
 
-    /** 사용자 선택 간의 시간. */
+    /** Time between changes in user selection. */
     private Cooldown selectionCooldown;
-    /** 1P, 2P 모드 확인 */
+    /** Check 1P, 2P Mode */
     public static int gameMode = 1;
-    /** 스킬 모드 확인 */
+    /** Check Skill Mode */
     public static boolean skillModeOn = true;
-    /** 스페이스 키가 눌렸을 때 별도의 동작을 수행하는 변수 */
+    /** Separate function when space is down*/
     private boolean canEscape = false;
 
     /**
-     * 생성자, 화면의 속성을 설정합니다.
+     * Constructor, establishes the properties of the screen.
      *
-     * @param width  화면 너비.
-     * @param height 화면 높이.
-     * @param fps    초당 프레임, 게임이 실행되는 프레임 속도.
+     * @param width  Screen width.
+     * @param height Screen height.
+     * @param fps    Frames per second, frame rate at which the game is run.
      */
-    public SelectScreen(int width, int height, int fps) {
+    public SelectScreen(int width, int height, int fps){
         super(width, height, fps);
         this.returnCode = 8;
         this.selectionCooldown = Core.getCooldown(SELECTION_TIME);
@@ -35,9 +35,9 @@ public class SelectScreen extends Screen {
     }
 
     /**
-     * 동작을 시작합니다.
+     * Starts the action.
      *
-     * @return 다음 화면 코드.
+     * @return Next screen code.
      */
     public final int run() {
         super.run();
@@ -46,7 +46,7 @@ public class SelectScreen extends Screen {
     }
 
     /**
-     * 화면의 요소를 업데이트하고 이벤트를 확인합니다.
+     * Updates the elements on screen and checks for events.
      */
     protected final void update() {
         super.update();
@@ -54,51 +54,53 @@ public class SelectScreen extends Screen {
         draw();
         if (this.selectionCooldown.checkFinished()
                 && this.inputDelay.checkFinished()) {
-            if (!canEscape) {
-                if (inputManager.isKeyDown(KeyEvent.VK_ESCAPE)) {
+            if(!canEscape){
+                if(inputManager.isKeyDown(KeyEvent.VK_ESCAPE)){
                     this.returnCode = 1;
                     this.isRunning = false;
                 }
                 if (inputManager.isKeyDown(KeyEvent.VK_RIGHT)
-                        || inputManager.isKeyDown(KeyEvent.VK_D)
-                        || inputManager.isKeyDown(KeyEvent.VK_LEFT)
-                        || inputManager.isKeyDown(KeyEvent.VK_A)) {
-                    gameMode = (gameMode == 1) ? 2 : 1;
-                    this.selectionCooldown.reset();
-                }
-                if (inputManager.isKeyDown(KeyEvent.VK_SPACE)) {
+                        || inputManager.isKeyDown(KeyEvent.VK_D)|| inputManager.isKeyDown(KeyEvent.VK_LEFT)|| inputManager.isKeyDown(KeyEvent.VK_A)) {
+                    if(gameMode == 1) gameMode = 2;
+                    else gameMode= 1;
+                    this.selectionCooldown.reset();}
+                if (inputManager.isKeyDown(KeyEvent.VK_SPACE)){
                     SoundManager.playSound("SFX/S_MenuClick", "menu_select", false, false);
                     canEscape = true;
                     this.selectionCooldown.reset();
+
+
                 }
-            } else {
-                if (inputManager.isKeyDown(KeyEvent.VK_ESCAPE)) {
+            }
+            else{
+                if(inputManager.isKeyDown(KeyEvent.VK_ESCAPE)){
                     canEscape = false;
                     this.selectionCooldown.reset();
                 }
                 if (inputManager.isKeyDown(KeyEvent.VK_RIGHT)
-                        || inputManager.isKeyDown(KeyEvent.VK_D)
-                        || inputManager.isKeyDown(KeyEvent.VK_LEFT)
-                        || inputManager.isKeyDown(KeyEvent.VK_A)) {
+                        || inputManager.isKeyDown(KeyEvent.VK_D)|| inputManager.isKeyDown(KeyEvent.VK_LEFT)|| inputManager.isKeyDown(KeyEvent.VK_A)) {
                     skillModeOn = !skillModeOn;
                     this.selectionCooldown.reset();
                 }
-                if (inputManager.isKeyDown(KeyEvent.VK_SPACE)) {
+                if (inputManager.isKeyDown(KeyEvent.VK_SPACE)){
                     this.isRunning = false;
                     SoundManager.playSound("SFX/S_MenuClick", "menu_select", false, false);
                 }
+
             }
+
         }
     }
 
     /**
-     * 화면과 관련된 요소를 그립니다.
+     * Draws the elements associated with the screen.
      */
     private void draw() {
         drawManager.initDrawing(this);
 
-        drawManager.drawSelect2PModeAndSkillModeScreen(this, gameMode, skillModeOn, canEscape);
+        drawManager.drawSelect2PModeAndSkillModeScreen(this,gameMode,skillModeOn, canEscape);
 
         drawManager.completeDrawing(this);
     }
+
 }
